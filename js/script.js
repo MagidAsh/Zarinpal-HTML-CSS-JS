a5618118222b14c6cf9fe0a333a1705a85464101
Majid Ash <Magid.Ash.Si@gmail.com>
2024-05-26 10:48:08 -0700

    Add hamburger menu to the site

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const crossIcon = document.querySelector(".cross-icon");
+
+hamburgerIcon.addEventListener("click", function () {
+  hamburgerMenu.classList.add("show-hamburger-menu");
+});
+
+crossIcon.addEventListener("click", function () {
+  hamburgerMenu.classList.remove("show-hamburger-menu");
+});
