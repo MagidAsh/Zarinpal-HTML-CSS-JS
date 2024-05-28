2f147b9dcac774c9c7369a38fd19d496687d81ba
Majid Ash <Magid.Ash.Si@gmail.com>
2024-05-28 04:05:06 -0700

    Creating drop-down navigation bar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const crossIcon = document.querySelector(".cross-icon");
+const showProductMenu = document.querySelector("#show-product-menu");
+const showMoreMenu = document.querySelector("#show-more-menu");
 
 hamburgerIcon.addEventListener("click", function () {
   hamburgerMenu.classList.add("show-hamburger-menu");
@@ -8,4 +10,26 @@ hamburgerIcon.addEventListener("click", function () {
 
 crossIcon.addEventListener("click", function () {
   hamburgerMenu.classList.remove("show-hamburger-menu");
+});
+
+showProductMenu.addEventListener("mouseenter", function (e) {
+  const div = e.target.childNodes[5];
+  div.style.animation = "showUp 0.35s 1";
+  div.style.display = "flex";
+});
+
+showProductMenu.addEventListener("mouseleave", function (e) {
+  const div = e.target.childNodes[5];
+  div.style.display = "none";
+});
+
+showMoreMenu.addEventListener("mouseenter", function (e) {
+  const ul = e.target.childNodes[5];
+  ul.style.animation = "showUp 0.35s 1";
+  ul.style.display = "block";
+});
+
+showMoreMenu.addEventListener("mouseleave", function (e) {
+  const ul = e.target.childNodes[5];
+  ul.style.display = "none";
 });
